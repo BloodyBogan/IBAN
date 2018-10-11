ce8a7433777806b59831177fe82910da39d999ef
Michal Kaštan <41683416+BloodyBogan@users.noreply.github.com>
2018-10-11 23:01:38 +0200

    Update main.js

--- main.js
+++ main.js
@@ -5,12 +5,12 @@ var ibanGenerator = function() {
 
   first = Math.floor(Math.random() * 90) + 10;
   second = Math.floor(Math.random() * 100);
-  third = Math.floor(Math.random() * 10);
+  third = Math.floor(Math.random() * 10000);
   fourth = Math.floor(Math.random() * 10000);
   fifth = Math.floor(Math.random() * 10000);
   sixth = Math.floor(Math.random() * 10000);
 
-  generatedIban = first + ' 0' + second + '0 ' + '000' + third + ' ' + fourth + ' ' + fifth + ' ' + sixth
+  generatedIban = first + ' 0' + second + '0 ' + third + ' ' + fourth + ' ' + fifth + ' ' + sixth
 
 }
 
